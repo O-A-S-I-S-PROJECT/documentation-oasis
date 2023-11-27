53e21fd1605490148926a3bba2c343e4985cc047
liveasoaresloredo <livesoaresloredo@outlook.com>
2023-11-27 09:06:05 -0300

    docs: :memo: Remove svg
    Delete all svg from docusaurus

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'What does Oasis do?',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
         OASIS is an artificial intelligence for reading documents, questions and answers, along with an organizational platform where you can create groups and upload project documents and the area whether internal or not. All this locally for data security of the company and the areas thus speeding up the productivity and performance of information at work.
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Tchnologies used',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
       Our core technology uses Figma for design, Next.js and Prisma for the front-end, Python and FastAPI for the back-end, GPTForAll for AI, ChromaDB for data storage, and MySQL and PostgreSQL databases, all managed with Docker.
@@ -23,11 +23,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
